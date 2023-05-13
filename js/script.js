5c99c4f85e5fe0c298fa64c4e6384de57b7512d5
EC2 Default User <ec2-user@ip-172-31-13-114.ap-northeast-1.compute.internal>
2023-05-13 13:28:28 +0000

    スクロール追加

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-const swiper = new Swiper('.swiper', {
-  loop: true,
-
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-buton-prev',
-  }
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
 });
